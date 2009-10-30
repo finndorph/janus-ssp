57f8b51d58f0955c61fe1b32bc1a9cad1b498e91
jach@wayf.dk <jach@wayf.dk@bbeb1b64-08b9-11de-b993-7fcebbf76996>
2009-10-30 11:16:30 +0000

    Tagged version 1.3.0

---
+++ tags/v.1.3.0/lib/AdminUtil.php
@@ -0,0 +1,399 @@
+<?php
+/**
+ * Administration utilities 
+ *
+ * PHP version 5
+ *
+ * JANUS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core 
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @author     Sixto Martín, <smartin@yaco.es>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @since      File available since Release 1.0.0
+ */
+/**
+ * Administration utilities 
+ *
+ * The class implements various utilities udes in the administrator interface
+ * in the dashboard in JANUS. The functionality in this class will probably be
+ * changed in the future. So do not rely on them to be valid if you are
+ * extending JANUS. 
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @author     Sixto Martín, <smartin@yaco.es>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @see        Sspmod_Janus_Database
+ * @since      Class available since Release 1.0.0
+ * @todo       Refactor this class
+ * @todo       Put errorhandling into exceptions
+ * @todo       Put most functions into Sspmod_Janus_Entity
+ */
+class sspmod_janus_AdminUtil extends sspmod_janus_Database
+{
+    /**
+     * JANUS config
+     * @var SimpleSAML_Configuration
+     */
+    private $_config;
+
+    /**
+     * Creates a new administrator utility.
+     *
+     * @since Method available since Release 1.0.0
+     */
+    public function __construct()
+    {
+        $this->_config = SimpleSAML_Configuration::getConfig('module_janus.php');
+
+        // Send DB config to parent class
+        parent::__construct($this->_config->getValue('store'));
+    }
+    
+    /**
+     * Retrive all entities from database
+     *
+     * The method retrives all entities from the database together with the 
+     * newest revision id.
+     *
+     * @return false|array All entities from the database
+     */
+    public function getEntitiesByState($state)
+    {
+        $st = self::execute(
+            'SELECT `eid`, `entityid`, MAX(`revisionid`) AS `revisionid`, 
+                `created`  
+            FROM `'. self::$prefix .'entity` WHERE `state` = ? 
+            GROUP BY `entityid`;'
+        , array($state));
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error fetching all entities');
+            return false;
+        }
+
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rs;
+    }
+
+
+    /**
+     * Retrive all entities from database
+     *
+     * The method retrives all entities from the database together with the 
+     * newest revision id.
+     *
+     * @return false|array All entities from the database
+     */
+    public function getEntities()
+    {
+        $st = self::execute(
+            'SELECT `eid`, `entityid`, MAX(`revisionid`) AS `revisionid`, 
+                `created`  
+            FROM `'. self::$prefix .'entity` 
+            GROUP BY `entityid`;'
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error fetching all entities');
+            return false;
+        }
+
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rs;
+    }
+
+    /**
+     * Returns an array user ids that have permission to see the given entity
+     *
+     * @param string $eid The entity whose parmissions is to be returned
+     *
+     * @return bool|array The users that have permission to see the entity
+     * 
+     * @since Method available since Release 1.0.0  
+     * @TODO Rename to getPermission or similar
+     */
+    public function hasAccess($eid)
+    {
+        assert('is_string($eid)');
+
+        $st = self::execute(
+            'SELECT t3.`uid`, t3.`userid` 
+            FROM `'. self::$prefix .'hasEntity` AS t2,
+            `'. self::$prefix .'user` AS t3
+            WHERE t3.active = ? AND t2.uid = t3.uid AND t2.`eid` = ?;', 
+            array('yes', $eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error fetching all entities');
+            return false;
+        }
+
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rs;
+    }
+
+    /**
+     * Returns an array user ids that do not have permission to see the given
+     * entity
+     *
+     * @param string $eid The entity whose parmissions is to be returned
+     *
+     * @return bool|array The users that do not have permission to see the
+     * entity
+     * 
+     * @since Method available since Release 1.0.0  
+     * @TODO Rename to getNegativePermission or similar
+     */
+    public function hasNoAccess($eid)
+    {
+        assert('is_string($eid)');
+
+        $st = self::execute(
+            'SELECT DISTINCT(t3.`uid`), t3.`userid` 
+            FROM `'. self::$prefix .'hasEntity` AS t2, 
+                `'. self::$prefix .'user` AS t3
+            WHERE t3.`uid` NOT IN (
+                SELECT uid
+                FROM `'. self::$prefix .'hasEntity`
+                WHERE `eid` = ?				
+            ) AND t3.`active` = ?;', 
+            array($eid, 'yes')
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error fetching all entities');
+            return false;
+        }
+
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rs;
+    }
+
+    /**
+     * Removes the specified users from the entity
+     * 
+     * @param string $eid The entity
+     * @param string $uid The user to be removed from the entity
+     *
+     * @return bool True on success and false on error
+     * 
+     * @since Method available since Release 1.0.0  
+     * @TODO Rename to removePermission or similar
+     */
+    public function removeUserFromEntity($eid, $uid)
+    {
+        $st = self::execute(
+            'DELETE FROM `'. self::$prefix .'hasEntity` 
+            WHERE `eid` = ? AND `uid` = ?;', 
+            array($eid, $uid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error removing the entity-user');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get entities from specified user
+     *
+     * @param string $uid The user
+     *
+     * @return array on success and false on error
+     * @since Method available since Release 1.2.0
+     */
+    public function getEntitiesFromUser($uid)
+    {
+        $st = self::execute(
+            'SELECT `eid` FROM `'. self::$prefix .'hasEntity`
+            WHERE `uid` = ?;',
+            array($uid)
+        );
+
+        if ($st === false) {
+             SimpleSAML_Logger::error('JANUS: Error returning the entities-user');
+             return false;
+        }
+        
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rs;
+    }
+
+
+   /**
+    * Remove all entities from a user
+    *
+    * @param string $uid The user to be removed from the entity
+    *
+    * return bool True on success and false on error
+    * @since Method available since Release 1.2.0
+    */
+    public function removeAllEntitiesFromUser($uid)
+    {
+    	$st = self::execute(
+            'DELETE FROM `'. self::$prefix .'hasEntity`
+            WHERE  `uid` = ?;',
+            array($uid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error removing all entities-user');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Add the specified users to the entity
+     * 
+     * @param string $eid The entity
+     * @param string $uid The user to be added to the entity
+     *
+     * @return bool True on success and false on error
+     * 
+     * @since Method available since Release 1.0.0  
+     * @TODO Rename to addPermission or similar
+     */
+    public function addUserToEntity($eid, $uid)
+    {
+        $st = self::execute(
+            'INSERT INTO `'. self::$prefix .'hasEntity` 
+                (`uid`, `eid`, `created`, `ip`)
+            VALUES 
+                (?, ?, ?, ?);',
+            array($uid, $eid, date('c'), $_SERVER['REMOTE_ADDR'])
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error('JANUS: Error fetching all entities');
+            return false;
+        }
+
+        $user = new sspmod_janus_User($this->_config->getValue('store'));
+        $user->setUid($uid);
+        $user->load();
+        $userid = $user->getUserid();
+
+        return $userid;
+    }
+
+    public function getAllowedTypes() {
+        $config = $this->_config;
+        $enablematrix = array(
+            'saml20-sp' => array(
+                'enable' => $config->getBoolean('enable.saml20-sp', false),
+                'name' => 'SAML 2.0 SP',
+            ),
+            'saml20-idp' => array(
+                'enable' => $config->getBoolean('enable.saml20-idp', false),
+                'name' => 'SAML 2.0 IdP',
+            ),
+            'shib13-sp' => array(
+                'enable' => $config->getBoolean('enable.shib13-sp', false),
+                'name' => 'Shib 1.3 SP',
+            ),
+            'shib13-idp' => array(
+                'enable' => $config->getBoolean('enable.shib13-idp', false),
+                'name' => 'Shib 1.3 IdP',
+            ),
+        );
+
+        return $enablematrix;
+    }
+
+    public function deleteEntity($eid) {
+        $st = $this->execute(
+            'DELETE FROM '. self::$prefix .'entity 
+            WHERE `eid` = ?;',
+            array($eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error(
+                'JANUS:deleteEntity - Not all revisions of entity deleted.'
+            );
+        }
+        
+        $st = $this->execute(
+            'DELETE FROM '. self::$prefix .'hasEntity 
+            WHERE `eid` = ?;',
+            array($eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error(
+                'JANUS:deleteEntity - Not all revisions of entity deleted.'
+            );
+        }
+        
+        $st = $this->execute(
+            'DELETE FROM '. self::$prefix .'metadata 
+            WHERE `eid` = ?;',
+            array($eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error(
+                'JANUS:deleteEntity - Not all revisions of entity deleted.'
+            );
+        }
+        
+        $st = $this->execute(
+            'DELETE FROM '. self::$prefix .'attribute 
+            WHERE `eid` = ?;',
+            array($eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error(
+                'JANUS:deleteEntity - Not all revisions of entity deleted.'
+            );
+        }
+        
+        $st = $this->execute(
+            'DELETE FROM '. self::$prefix .'blockedEntity 
+            WHERE `eid` = ?;',
+            array($eid)
+        );
+
+        if ($st === false) {
+            SimpleSAML_Logger::error(
+                'JANUS:deleteEntity - Not all revisions of entity deleted.'
+            );
+        }
+        return; 
+    }
+}
+?>
---
+++ tags/v.1.3.0/lib/UserController.php
@@ -0,0 +1,316 @@
+<?php
+/**
+ * Controller for users
+ *
+ * PHP version 5
+ *
+ * JANUS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * JANUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JANUS. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @since      File available since Release 1.0.0
+ */
+/**
+ * Controller for users
+ *
+ * Basic functionality for handling user like added users to entities.
+ *
+ * @category   SimpleSAMLphp
+ * @package    JANUS
+ * @subpackage Core
+ * @author     Jacob Christiansen <jach@wayf.dk>
+ * @copyright  2009 Jacob Christiansen 
+ * @license    http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/janus-ssp/
+ * @since      Class available since Release 1.0.0
+ */
+class sspmod_janus_UserController extends sspmod_janus_Database
+{
+    /**
+     * Configuration
+     *
+     * @var SimpleSAML_Configuration
+     */
+    private $_config;
+
+    /**
+     * User
+     * 
+     * @var sspmod_janus_User
+     */
+    private $_user;
+
+    /**
+     * List of user connected entities
+     * @var array List of sspmod_janus_Entity
+     */
+    private $_entities;
+
+    /**
+     * Create a new user controller
+     *
+     * @param SimpleSAML_Configuration &$config JANUS configuration
+     *
+     * @since Method available since Release 1.0.0
+     */
+    public function __construct(SimpleSAML_Configuration &$config)
+    {
+        // Send DB config to parent class
+        parent::__construct($config->getValue('store'));
+        $this->_config = $config;
+    }
+
+    /**
+     * Set the user for the user to be handled
+     *
+     * @param string|sspmod_janus_User $user The user email or a user object
+     *
+     * @return sspmod_janus_User|bool Return the user or false if the user can
+     * not be loaded
+     * @throws InvalidArgumentException If parsed argument is neither a valid 
+     * email address og a user object
+     * @since Method available since Release 1.0.0
+     */
+    public function setUser($user)
+    {
+        // If $user is an email address
+        if (is_string($user)) {
+            $this->_user = new sspmod_janus_User($this->_config->getValue('store'));
+            $this->_user->setUserid($user);
+            if (!$this->_user->load(sspmod_janus_User::USERID_LOAD)) {
+                return false;
+            }
+            // If $user is a sspmod_janus_User object
+        } else if (is_a($user, 'sspmod_janus_User')) {
+            $this->_user = $user;
+        } else {
+            throw new InvalidArgumentException(
+                'Argument must be an email address or instance of sspmod_janus_User.'
+            );
+        }
+        return $this->_user;
+    }
+
+    /**
+     * Load entities that user has access to
+     *
+     * @return bool True on success and false on error.
+     * @since Method available since Release 1.0.0
+     */
+    private function _loadEntities()
+    {
+        $st = $this->execute(
+            'SELECT * FROM '. self::$prefix .'hasEntity WHERE `uid` = ?;',
+            array($this->_user->getUid())
+        );
+
+        if ($st === false) {
+            return false;	
+        }
+
+        $this->_entities = array();
+        $rs = $st->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rs AS $row) {
+            $entity = new sspmod_janus_Entity($this->_config->getValue('store'));
+            $entity->setEid($row['eid']);
+            if ($entity->load()) {
+                $this->_entities[] = $entity;
+            } else {
+                SimpleSAML_Logger::error(
+                    'JANUS:UserController:_loadEntities - Entity could not be
+                    loaded, entity id: '.$row['entityid']
+                );
+            }
+        }
+        return true;	
+    }
+
+    /**
+     * Return the entities that the user has access to
+     *
+     * @param bool $force Force the method to reload the list of entities
+     *
+     * @return bool|array Array of sspmod_janus_Entity or false on error
+     * @since Method available since Release 1.0.0
+     */
+    public function getEntities($force = false)
+    {
+        assert('is_bool($force);');
+
+        if (empty($this->_entities) || $force) {
+            if (!$this->_loadEntities()) {
+                return false;
+            }
+        }
+        return $this->_entities;
+    }
+
+    /**
+     * Create new entity with parsed entityid
+     *
+     * Create a new entity and give the user access to the entity.
+     *
+     * @param string $entityid Entity id for the new entity
+     *
+     * @return sspmod_janus_Entity|bool Returns the entity or false on error.
+     * @since Method available since Release 1.0.0
+     */
+    public function createNewEntity($entityid, $type)
+    {
+        assert('is_string($entityid)');
+        assert('is_string($type)');
+
+        // Check if the entity id is already used
+        $st = $this->execute(
+            'SELECT count(*) AS count 
+            FROM '. self::$prefix .'entity 
+            WHERE `entityid` = ?;',
+            array($entityid)
+        );
+
+        if ($st === false) {
+            return 'error_db';
+        }
+
+        $row = $st->fetchAll(PDO::FETCH_ASSOC);
+        if ($row[0]['count'] > 0) {
+            return 'error_entity_exists';
+        }
+
+        $startstate = $this->_config->getString('workflowstate.default');
+
+        // Instanciate new entity
+        $entity = new sspmod_janus_Entity($this->_config->getValue('store'), true);
+        $entity->setEntityid($entityid);
+        $entity->setWorkflow($startstate);
+        $entity->setType($type);
+        $entity->setRevisionnote('Entity created.');
+        $entity->save();
+        
+        $st = $this->execute(
+            'INSERT INTO '. self::$prefix .'hasEntity 
+            (`uid`, `eid`, `created`, `ip`) 
+            VALUES 
+            (?, ?, ?, ?);', 
+            array(
+                $this->_user->getUid(),
+                $entity->getEid(),
+                date('c'),
+                $_SERVER['REMOTE_ADDR'],
+            )
+        );
+
+        if ($st === false) {
+            return 'error_db';
+        }
+
+        $ec = new sspmod_janus_EntityController($this->_config);
+        $ec->setEntity($entity);
+
+        $update = false;
+        foreach($this->_config->getValue('metadatafields.'.$type) AS $mk => $mv) {
+            if(isset($mv['required']) && $mv['required'] === true)
+            {
+                $ec->addMetadata($mk, $mv['default']);
+                $update = true;
+            }
+        }
+
+        if($update === true)
+        {
+            $ec->saveEntity();
+        }
+
+        // Reset list of entities
+        $this->_entities = null;
+        $this->_loadEntities();
+        
+        return $entity->getEid();
+    }
+
+    /**
+     * Return the user
+     *
+     * @return sspmod_janus_Use
+     * @see    sspmod_janus_User 
+     * @since  Method available since Release 1.0.0
+     */
+    public function getUser()
+    {
+        return $this->_user;
+    }
+    
+    /**
+     * Retrive all active users in the system
+     *
+     * The method will retrive all active users in the system. NOTE this method will be
+     * moved/rewritten in the future.
+     *
+     * @return array All active users in the system
+     * @since  Method available since Release 1.2.0
+     */
+    public function getActiveUsers()
+    {
+        $st = $this->execute(
+            'SELECT * FROM '. self::$prefix .'user WHERE `active` = ?;',
+            array('yes')
+        );
+
+        return $st->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    /**
+     * Retrive all users in the system
+     *
+     * The method will retrive all users in the system. NOTE this method will be
+     * moved/rewritten in the future.
+     *
+     * @return array All users in the system
+     */
+    public function getUsers()
+    {
+        $st = $this->execute('SELECT * FROM '. self::$prefix .'user;');
+
+        return $st->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * Erases all entities in database
+     *
+     * Erases all entities and related metadata, attributes and blocked 
+     * entities. NOTE this method is only for developing purpose
+     *
+     * @return True ALways return true, no matter if the DB calls fails or not
+     */
+    public function truncateDB()
+    {
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'entity;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'hasEntity;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'metadata;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'attribute;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'blockedEntity;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'message;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'subscriptions;');
+        $st = $this->execute('TRUNCATE TABLE '. self::$prefix .'tokens;');
+        return true;
+    }
+}
+?>
